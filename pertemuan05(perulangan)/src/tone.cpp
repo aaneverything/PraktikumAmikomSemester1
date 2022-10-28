d575ec6f70df4305af2f21e7421e694d4e231c77
rizpedia <achmadrizkiramadhan0101@gmail.com>
2022-10-28 09:56:01 +0700

    nada menggunakan keyboard

---
+++ pertemuan05(perulangan)/src/tone.cpp
@@ -0,0 +1,41 @@
+// Progaram : toon Generator Nada
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        char tone = getch();
+        double freq = 0;
+        switch (tone)
+        {
+        case 'c':
+            freq = 261.63;
+            break;
+        case 'd':
+            freq = 293.66;
+            break;
+        case 'e':
+            freq = 329.63;
+            break;
+        case 'f':
+            freq = 349.23;
+            break;
+        case 'g':
+            freq = 392.00;
+            break;
+        case 'a':
+            freq = 440.00;
+            break;
+        case 'b':
+            freq = 493.88;
+            break;
+        }
+        Beep(freq, 500);
+    }
+
+    return 0;
+}
